f51adfc4995e256833d856cf5b00cec9395a9aef
Jim Bagrow <bagrowjp@gmail.com>
2020-01-24 14:05:26 -0500

    refactor fig1 plotting script to avoid chdir and better tight_layout

--- figure1-density.py
+++ figure1-density.py
@@ -3,9 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-os.chdir("ER_BA_N1000/processing/")
-ER = pd.read_csv("hx_ER.csv")
-BA = pd.read_csv("hx_BA.csv")
+dir1 = "ER_BA_N1000/processing"
+dir2 = "real_networks/processing/analyses-2019-10-29"
+ER = pd.read_csv(f"{dir1}/hx_ER.csv")
+BA = pd.read_csv(f"{dir1}/hx_BA.csv")
 N = 1000
 BAdeg = [2*m - 2*m**2/N for m in BA["m"].values] # theoretical degree for BA
 ##BAparam = list(range(1,21+1)) + list(range(22,25+1)) # for BA -- hack'ey way of better spacing the x-axis
@@ -29,8 +30,7 @@ plt.xlabel(r"$\langle k \rangle$")
 plt.ylabel(r"Variance of $h_\times$")
 
 # Real networks: cross-entropy versus density & other network statistics
-os.chdir("../../real_networks/processing/analyses-2019-10-29")
-df = pd.read_csv("real_networks-links_only.csv")
+df = pd.read_csv(f"{dir2}/real_networks-links_only.csv")
 
 plt.sca(ax[2])
 plt.plot(df["density"].values,df["average_hx"].values,'o')
@@ -45,7 +45,7 @@ for i,network in enumerate(small_networks):
     y = df["average_hx"].values[i]
 
     if network == "Freeman's EIES":
-        plt.text(x-0.22, y-0.01, network, fontsize=8.5)
+        plt.text(x, y-0.01*1.5, network.replace(" ", "\n"), fontsize=8.5, ha='center')
     else:
         plt.text(x+.02, y, network, fontsize=8.5)
 
@@ -54,9 +54,9 @@ plt.ylabel(r"Average cross-entropy, $\langle h_\times \rangle$")
 
 
 
-plt.tight_layout()
-plt.savefig("../../../figure1.pdf")
-plt.show()
+plt.tight_layout(w_pad=0.5)
+plt.savefig("figure1-test.pdf")
+# plt.show()
 
 
 
