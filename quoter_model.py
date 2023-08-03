12d6f2d841018c508ad445f66ccfd4ab74266295
jamie <jsm89@cantab.ac.uk>
2023-08-03 07:27:52 +0100

    moved all common scripts to project root

--- quoter_model.py
+++ quoter_model.py
@@ -10,6 +10,7 @@ import math
 import random
 import os, sys
 from CrossEntropy import *
+from edge_clustering_coeff import *
 
 def words_to_tweets(words,times):
     # Convert (words,times) array to a smaller array of (tweets,times)  
@@ -25,20 +26,43 @@ def write_all_data(G,outdir,outfile):
     """
         Compute and write data from quoter model simulations.
     """
+
+    # graph skeleton for calculating clustering, transitivity, ASPL, etc.
+    H = G.to_undirected()
+    
     # compute edge data
-    for e in G.edges():
-        # compute all cross entropies. e[0] = alter, e[1] = ego 
+    edges = random.sample(G.edges(),min(500,nx.number_of_edges(G)))
+    nonedges = random.sample(list(nx.non_edges(G)),min(500,len(list(nx.non_edges(G)))) )
+
+    edge_sample = edges + nonedges
+    alter_list, ego_list, qp_list, hx_list, dist_list=[],[],[],[],[]
+    tri_list, alter_degs, ego_degs = [],[],[]
+
+    for e in edge_sample:
+        # compute cross entropies. e[0] = alter, e[1] = ego 
         time_tweets_target = words_to_tweets(G.node[e[1]]["words"],G.node[e[1]]["times"])
         time_tweets_source = words_to_tweets(G.node[e[0]]["words"],G.node[e[0]]["times"])
         hx = timeseries_cross_entropy(time_tweets_target, time_tweets_source, please_sanitize=False)
-        G[e[0]][e[1]]["hx"] = hx
+        hx_list.append(hx)
+        alter_list.append(e[0])
+        ego_list.append(e[1])
 
         # also record quote probability
-        G[e[0]][e[1]]["quoteProb"] = 1/len(G.predecessors(e[1]))
-        
-    # graph skeleton for calculating clustering, transitivity, ASPL, etc.
-    H = G.to_undirected()
+        qp_list.append(1/len(G.predecessors(e[1])))
 
+        # also record edge embeddeness & edge clustering coefficient
+        triangles, deg0, deg1, ECC = edge_clustering_coeff(H,e[0],e[1],return_info=True)
+        tri_list.append(triangles)
+        alter_degs.append(deg0)
+        ego_degs.append(deg1)
+
+        # also record distance between nodes
+        try:
+            dist = nx.shortest_path_length(G,source=e[0],target=e[1])
+        except:
+            dist = -1
+        dist_list.append(dist)
+        
     # compute graph data
     nnodes = nx.number_of_nodes(H)
     nedges = nx.number_of_edges(H)
@@ -46,40 +70,39 @@ def write_all_data(G,outdir,outfile):
     indegs = list(G.in_degree(G.nodes()).values())
     outdegs = list(G.out_degree(G.nodes()).values())
     ccs = sorted(nx.connected_components(H), key=len, reverse=True)
-
-    try: # error is raised if graph is disconnected
-        avg_shortest_path = nx.average_shortest_path_length(H)
-    except:
-        avg_shortest_path = 0
     
     data_tuple = (nnodes,nedges,dens,np.mean(indegs), np.min(indegs), np.max(indegs),
                       np.min(outdegs), np.max(outdegs), nx.transitivity(H), nx.average_clustering(H),
-                      avg_shortest_path, nx.degree_assortativity_coefficient(H), 
+                    nx.degree_assortativity_coefficient(H), 
                     len(ccs), len(ccs[0])) # note avg_in == avg_out, so we only need to record one
 
     # write graph data
     with open(outdir + "graph/" + outfile, "w") as f:
         f.write("nodes edges density average_degree min_indegree max_indegree " +
                 "min_outdegree max_outdegree transitivity average_clustering " +
-                "average_shortest_path_length assortativity " +
+                "assortativity " +
                 "number_of_components largest_component\n") # header
         
-        f.write("%i %i %0.8f %0.8f %i %i %i %i %0.8f %0.8f %0.8f %0.8f %i %i" % data_tuple)
+        f.write("%i %i %0.8f %0.8f %i %i %i %i %0.8f %0.8f %0.8f %i %i" % data_tuple)
 
     # write edge data
     with open(outdir + "edge/" + outfile, "w") as f:
-        f.write("alter ego quoteProb hx\n") # header
-        for e in G.edges():
-            f.write("%i %i %0.8f %0.8f\n" % (e[0], e[1], G[e[0]][e[1]]["quoteProb"], G[e[0]][e[1]]["hx"]) )
-
+        f.write("alter ego quoteProb hx distance triangles alter_deg ego_deg\n") # header
+        for i in range(len(hx_list)):
+            data_tuple = (alter_list[i],ego_list[i],qp_list[i],hx_list[i],dist_list[i],tri_list[i],alter_degs[i],ego_degs[i])
+            f.write("%i %i %0.8f %0.8f %i %i %i %i\n" % data_tuple)
+    
     # write node data
     with open(outdir + "node/" + outfile, "w") as f:
-        f.write("node indegree outdegree C\n") # header
+        f.write("node indegree outdegree C h\n") # header
         for node in G.nodes():
+            time_tweets_target = words_to_tweets(G.node[node]["words"],G.node[node]["times"])
+            time_tweets_source = words_to_tweets(G.node[node]["words"],G.node[node]["times"])
+            h = timeseries_cross_entropy(time_tweets_target, time_tweets_source, please_sanitize=False)
             indeg = G.in_degree(node)
             outdeg = G.out_degree(node)
             C = nx.clustering(H, node)
-            f.write("%i %i %i %0.8f\n" % (node,indeg,outdeg,C))
+            f.write("%i %i %i %0.8f %0.8f\n" % (node,indeg,outdeg,C,h))
 
 def quoter_model_sim(G,q,T,outdir,outfile,write_data=write_all_data,dunbar=None):
     """ Simulate the quoter model on a graph G. Nodes take turns generating content according to two
