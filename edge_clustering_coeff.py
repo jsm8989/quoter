2cb24e1685afffc84e8f2c0772c5b8c72d1eb024
jamie <jsm89@cantab.ac.uk>
2023-08-23 13:26:36 +0100

    improved and started running ECC examples

--- edge_clustering_coeff.py
+++ edge_clustering_coeff.py
@@ -3,31 +3,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def edge_clustering_coeff(G: nx.Graph, u: int, v: int, return_info: bool = False):
+def edge_clustering_coeff(
+    G: nx.Graph, u: int, v: int, return_info: bool = False, draw: bool = False
+):
     """
-    Compute ECC between two nodes.
+    Compute ECC between two nodes u and v, defined as the number of triangles containing both u and v divided by min(degrees(u,v))-1
 
     Args:
         G: NetworkX graph to be analysed. Must be directed
         u: node index of first node
         v: node index of second node
         return_info: if True return information about the algorithm
+        draw: choose whether to visualise the graph
 
     Returns:
-        triangles deg_u deg_v ECC as
+        triangles deg_u deg_v ECC (if return_info)
+        ECC
     """
     u_nbrs = nx.neighbors(G, u)
     v_nbrs = nx.neighbors(G, v)
     uv_nbrs = set(u_nbrs) & set(v_nbrs)
-    triangles = len(uv_nbrs)
+    triangles = len(uv_nbrs)  # could be replaced by nx.triangles(G, [u,v]) or similar
 
-    deg_u = len(u_nbrs)
-    deg_v = len(v_nbrs)
+    deg_u = nx.degree(G)[u]  # len(u_nbrs)
+    deg_v = nx.degree(G)[v]  # len(v_nbrs)
 
     if min(deg_u - 1, deg_v - 1) == 0:  # undefined?
         ECC: float = 0
     else:
         ECC = triangles / min(deg_u - 1, deg_v - 1)
+
+    if draw:
+        pos = nx.spring_layout(G)
+        labels = nx.draw_networkx_labels(G, pos)
+        nx.draw(G, pos)
+        plt.show()
 
     if return_info:
         return triangles, deg_u, deg_v, ECC
@@ -53,20 +63,16 @@ if __name__ == "__main__":
             (5, 6),
         ]
     )
-    print(edge_clustering_coeff(G, 1, 4))
-    pos = nx.spring_layout(G)
-    labels = nx.draw_networkx_labels(G, pos)
-    nx.draw(G, pos)
-    plt.show()
+    print(edge_clustering_coeff(G, 1, 4, draw=True))
 
-    # Test on real networks
-##    for name in networks_dict:
-##        print(name)
-##        G = read_any(name)
-##        ECCs = []
-##        for i,e in enumerate(G.edges()):
-##            ECCs.append(edge_clustering_coeff(G,e[0],e[1]))
-##    ##    print(len([x for x in ECCs if x == 2])/len(G.edges()))
-##        print(np.mean([x for x in ECCs if x != 2]))
-##    ##plt.hist([x for x in ECCs if x !=2])
-##    ##plt.show()
+    # Test on real networks - TODO
+    # for name in networks_dict:
+    #     print(name)
+    #     G = read_any(name)
+    #     ECCs = []
+    #     for i, e in enumerate(G.edges()):
+    #         ECCs.append(edge_clustering_coeff(G, e[0], e[1]))
+    #     print(len([x for x in ECCs if x == 2]) / len(G.edges()))
+    #     print(np.mean([x for x in ECCs if x != 2]))
+    # plt.hist([x for x in ECCs if x != 2])
+    # plt.show()
